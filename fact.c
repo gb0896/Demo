53a0db27f95b9d057e74c5b69e367d07e85de5a6
gunjan balde <gunjan.balde@gmail.com>
2017-09-12 17:01:01 +0530

    Adding the files main, add, fact

---
+++ fact.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int fact(int n){
+int ans=1;
+	for(int i=1;i<=n;i++)
+		ans=ans*i;
+return ans;
+}
---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "add.c"
+#include "sub.c"
+#include "mul.c"
+#include "power.c"
+#include "fact.c"
+
+int main(){
+        int a,b;
+        scanf("%d%d",&a,&b);
+        printf("Addition = %d",add(a,b));
+        printf("Substraction = %d",sub(a,b));
+        printf("Multiplication = %d",mul(a,b));
+        printf("Exponent = %d",power(a,b));
+        printf("Factorial = %d",fact(a));
+}
